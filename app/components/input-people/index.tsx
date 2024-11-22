834abe3e9abb6fb389431db1cdf6bd49845dcf25
Nico Moccagatta <nicomoccagatta@gmail.com>
2024-11-22 12:36:28 -0300

    Add ability to remove person

--- app/components/input-people/index.tsx
+++ app/components/input-people/index.tsx
@@ -10,6 +10,8 @@ export default function InputPeople({
   const nameRef = useRef<HTMLInputElement>(null)
 
   const handleOKClick = () => {
+    if (!name) return
+
     onClick({ name, amount })
     setName("")
     setAmount(0)
@@ -17,10 +19,10 @@ export default function InputPeople({
   }
 
   return (
-    <form className="grid bg-blue-200 p-4" onClick={e => e.preventDefault()}>
+    <form className="grid bg-blue-200 p-4 w-96 h-96" onClick={e => e.preventDefault()}>
       <input
         autoFocus
-        className="mb-2 text-black"
+        className="mb-2 text-black h-12"
         type="text"
         name="name"
         placeholder="Nombre"
@@ -29,14 +31,15 @@ export default function InputPeople({
         ref={nameRef}
       />
       <input
-        className="mb-2 text-black"
+        className="mb-2 text-black h-12"
         type="number"
         name="amount"
         placeholder="Cuanto gasto?"
         onChange={e => setAmount(Number(e.target.value))}
         value={amount}
       />
-      <button className="bg-blue-800" onClick={handleOKClick}>Agregar</button>
+      <button className="bg-blue-800 h-12" onClick={handleOKClick}>Agregar</button>
+      <div className="min-h-48" />
     </form>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -10,21 +10,75 @@ function reducer(state, action) {
       people: [
         ...state.people,
         { name, amount },
-      ]
+      ],
+      amount: state.amount + amount,
+    }
+  }
+
+  if (action.type === 'delete_person') {
+    const newAmount = state.amount - state.people[action.idx].amount
+    const newPeople = [...state.people]
+    newPeople.splice(action.idx, 1)
+    return {
+      ...state,
+      amount: newAmount,
+      people: newPeople,
     }
   }
 
   throw Error('Unknown action.')
 }
 
-export default function Home() {
-  const [state, dispatch] = useReducer(reducer, { people: [] })
+const initialState = {
+  people: [],
+  amount: 0,
+}
 
+export default function Home() {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const personBill = Math.round(state.amount / state.people.length)
+
+  console.log('[DEBUG]', JSON.stringify(state,null,2))
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <InputPeople onClick={person => dispatch({ type: 'add_person', person }) } />
-        <p>Personas:</p> {state.people.map(({ name, amount }) => (<li key={`${name}-${amount}`}>{name}: {amount}</li>))}
+        <div className="flex gap-12">
+          <InputPeople onClick={person => dispatch({ type: 'add_person', person }) } />
+          <div className="w-96 h-96">
+            <div><p>Total: ${state.amount} {personBill ? '($' + personBill + ' cada uno)' : ''}</p></div>
+            <p className="mt-4 mb-4">Personas</p>
+            {state.people.map(
+              ({ name, amount }: { name: string, amount: number }, idx: number) => {
+                const bill = amount - personBill
+                if (bill === 0) {
+                  return (
+                    <li key={`${name}-${amount}`}>
+                      {name} pagó {amount}
+                      <button
+                        onClick={() => dispatch({ type: 'delete_person', idx })}
+                        className="ml-4 rounded-xl p-1 bg-white text-black"
+                      >
+                        Eliminar
+                      </button>
+                    </li>
+                  )
+                }
+                const status = bill > 0 ? 'debe recibir' : 'debe pagar'
+                return (
+                  <li className="mb-2" key={`${name}-${amount}`}>
+                    {name} pagó {amount}, {status} {Math.abs(bill)}
+                    <button
+                      onClick={() => dispatch({ type: 'delete_person', idx })}
+                      className="ml-4 rounded-xl p-1 bg-white text-black"
+                    >
+                      Eliminar
+                    </button>
+                  </li>
+                )
+              })
+            }
+          </div>
+        </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
