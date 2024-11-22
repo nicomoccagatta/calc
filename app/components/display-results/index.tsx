5e8e9ce6d783a730948282db7338c9d8fb375ebb
Nico Moccagatta <nicomoccagatta@gmail.com>
2024-11-22 13:31:23 -0300

    Add copy to clipboard feature

--- app/components/display-results/index.tsx
+++ app/components/display-results/index.tsx
@@ -7,6 +7,13 @@ type tProps = {
   totalAmount: number,
 }
 
+const copyClipboard = () => {
+  const copyText = document.getElementById("display-results")
+  if (copyText?.textContent) {
+    navigator.clipboard.writeText(copyText?.innerText.replaceAll("\n\n", "\n").replaceAll("Eliminar", ""));
+  }
+}
+
 export default function DisplayResults({
   dispatch,
   people,
@@ -14,39 +21,42 @@ export default function DisplayResults({
   totalAmount,
 } : tProps) {
   return (
-    <div className="w-96 h-96">
-      <div><p>Total: ${totalAmount} {personBill ? '($' + personBill + ' cada uno)' : ''}</p></div>
-      <p className="mt-4 mb-4">Personas</p>
-      {people.map(
-        ({ name, amount }: { name: string, amount: number }, idx: number) => {
-          const bill = amount - personBill
-          if (bill === 0) {
+    <div className="grid w-max">
+      <div className="" id="display-results">
+        <p>Total: ${totalAmount} {personBill ? '($' + personBill + ' cada uno)' : ''}</p>
+        <p className="mt-4 mb-4">Personas</p>
+        {people.map(
+          ({ name, amount }: { name: string, amount: number }, idx: number) => {
+            const bill = amount - personBill
+            if (bill === 0) {
+              return (
+                <li key={`${name}-${amount}`}>
+                  ✅ {name} pagó {amount}
+                  <button
+                    onClick={() => dispatch({ type: 'delete_person', idx })}
+                    className="ml-4 select-none rounded-xl p-1 bg-white text-black"
+                  >
+                    Eliminar
+                  </button>
+                </li>
+              )
+            }
+            const status = bill > 0 ? 'debe recibir' : 'debe pagar'
             return (
-              <li key={`${name}-${amount}`}>
-                {name} pagó {amount}
+              <li className="mb-2" key={`${name}-${amount}`}>
+                ❌ {name} pagó {amount}, {status} {Math.abs(bill)}
                 <button
                   onClick={() => dispatch({ type: 'delete_person', idx })}
-                  className="ml-4 rounded-xl p-1 bg-white text-black"
+                  className="ml-4 select-none rounded-xl p-1 bg-white text-black"
                 >
                   Eliminar
                 </button>
               </li>
             )
-          }
-          const status = bill > 0 ? 'debe recibir' : 'debe pagar'
-          return (
-            <li className="mb-2" key={`${name}-${amount}`}>
-              {name} pagó {amount}, {status} {Math.abs(bill)}
-              <button
-                onClick={() => dispatch({ type: 'delete_person', idx })}
-                className="ml-4 rounded-xl p-1 bg-white text-black"
-              >
-                Eliminar
-              </button>
-            </li>
-          )
-        })
-      }
+          })
+        }
+      </div>
+      <button className="bg-white text-black p-2 rounded-xl mt-8 max-h-12" onClick={copyClipboard}>📋 Copy text</button>
     </div>
   )
 }
